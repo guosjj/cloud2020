ac2214e690235131fe516ca939456c3552c4b814
“guosj” <2547361543@qq.com>
2022-06-30 19:28:20 +0800

    服务提供者注册到注册中心，开启负载均衡

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -1,7 +1,6 @@
 package com.atguigu.springcloud.dao;
 
 import com.atguigu.springcloud.entities.Payment;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
